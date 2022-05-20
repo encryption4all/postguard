ff94007e987ba2b73b06a7b54c351d19fe16e337
Leon <l.botros@cs.ru.nl>
2022-05-20 17:10:23 +0200

    refactor: use syntactic sugar macros for async main in cli and pkg

--- irmaseal-cli/src/main.rs
+++ irmaseal-cli/src/main.rs
@@ -2,21 +2,16 @@ mod client;
 mod decrypt;
 mod encrypt;
 mod opts;
-//mod util;
 
 use crate::opts::{Opts, Subcommand};
 use clap::Parser;
-use tokio::runtime::Runtime;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let opts = Opts::parse();
 
-    let mut rt = Runtime::new().unwrap();
-
-    rt.block_on(async {
-        match opts.subcmd {
-            Subcommand::Enc(o) => crate::encrypt::exec(o).await,
-            Subcommand::Dec(o) => crate::decrypt::exec(o).await,
-        }
-    });
+    match opts.subcmd {
+        Subcommand::Enc(o) => crate::encrypt::exec(o).await,
+        Subcommand::Dec(o) => crate::decrypt::exec(o).await,
+    }
 }
--- irmaseal-pkg/src/server.rs
+++ irmaseal-pkg/src/server.rs
@@ -6,11 +6,11 @@ use crate::handlers;
 use crate::opts::*;
 use crate::util::*;
 use actix_cors::Cors;
-use actix_rt::System;
 use actix_web::{
     http::header,
     web,
     web::{resource, scope, Data},
+    App, HttpServer,
 };
 
 #[derive(Clone)]
@@ -19,7 +19,8 @@ pub struct MasterKeyPair<K: IBKEM> {
     pub sk: K::Sk,
 }
 
-pub fn exec(server_opts: ServerOpts) {
+#[actix_rt::main]
+pub async fn exec(server_opts: ServerOpts) {
     let ServerOpts {
         host,
         port,
@@ -33,61 +34,57 @@ pub fn exec(server_opts: ServerOpts) {
         sk: cgwkv_read_sk(secret).unwrap(),
     };
 
-    System::new().block_on(async move {
-        let jwt_pk_bytes = reqwest::get(&format!("{irma}/publickey"))
-            .await
-            .expect("could not retrieve JWT public key")
-            .bytes()
-            .await
-            .expect("could not retrieve JWT public key bytes");
-
-        let decoding_key =
-            DecodingKey::from_rsa_pem(&jwt_pk_bytes).expect("could not parse JWT public key");
-
-        actix_web::HttpServer::new(move || {
-            actix_web::App::new()
-                .wrap(
-                    Cors::default()
-                        .allow_any_origin()
-                        .allowed_methods(vec!["GET", "POST"])
-                        .allowed_header(header::CONTENT_TYPE)
-                        .allowed_header(header::AUTHORIZATION)
-                        .allowed_header("X-Postguard-Client-Version")
-                        .max_age(3600),
-                )
-                .app_data(Data::new(
-                    actix_web::web::JsonConfig::default().limit(1024 * 4096),
-                ))
-                .service(
-                    scope("/v2")
-                        .service(
-                            resource("/parameters")
-                                .app_data(Data::new(kp.pk))
-                                .route(web::get().to(handlers::parameters::<CGWKV>)),
-                        )
-                        .service(
-                            resource("/request/start")
-                                .app_data(Data::new(irma.clone()))
-                                .route(web::post().to(handlers::request)),
-                        )
-                        .service(
-                            resource("/request/jwt/{token}")
-                                .app_data(Data::new(irma.clone()))
-                                .route(web::get().to(handlers::request_jwt)),
-                        )
-                        .service(
-                            resource("/request/key/{timestamp}")
-                                .app_data(Data::new(kp.sk))
-                                .app_data(Data::new(decoding_key.clone()))
-                                .route(web::get().to(handlers::request_key::<CGWKV>)),
-                        ),
-                )
-        })
-        .bind(format!("{}:{}", host, port))
-        .unwrap()
-        .shutdown_timeout(1)
-        .run()
+    let jwt_pk_bytes = reqwest::get(&format!("{irma}/publickey"))
         .await
-        .unwrap()
+        .expect("could not retrieve JWT public key")
+        .bytes()
+        .await
+        .expect("could not retrieve JWT public key bytes");
+
+    let decoding_key =
+        DecodingKey::from_rsa_pem(&jwt_pk_bytes).expect("could not parse JWT public key");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .allowed_header(header::AUTHORIZATION)
+                    .allowed_header("X-Postguard-Client-Version")
+                    .max_age(3600),
+            )
+            .app_data(Data::new(web::JsonConfig::default().limit(1024 * 4096)))
+            .service(
+                scope("/v2")
+                    .service(
+                        resource("/parameters")
+                            .app_data(Data::new(kp.pk))
+                            .route(web::get().to(handlers::parameters::<CGWKV>)),
+                    )
+                    .service(
+                        resource("/request/start")
+                            .app_data(Data::new(irma.clone()))
+                            .route(web::post().to(handlers::request)),
+                    )
+                    .service(
+                        resource("/request/jwt/{token}")
+                            .app_data(Data::new(irma.clone()))
+                            .route(web::get().to(handlers::request_jwt)),
+                    )
+                    .service(
+                        resource("/request/key/{timestamp}")
+                            .app_data(Data::new(kp.sk))
+                            .app_data(Data::new(decoding_key.clone()))
+                            .route(web::get().to(handlers::request_key::<CGWKV>)),
+                    ),
+            )
     })
+    .bind(format!("{}:{}", host, port))
+    .unwrap()
+    .shutdown_timeout(1)
+    .run()
+    .await
+    .unwrap()
 }
