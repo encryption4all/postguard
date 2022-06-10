be5da7cc68b6547686bd0cf0871eef6c44f12521
Leon <leon.botros@ru.nl>
2022-06-10 22:42:22 +0200

    cleanup: remove outdated docs

--- docs/generate.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-pushd assets
-./generate.sh
-popd
-
-pandoc -f markdown -t html5 --highlight-style=haddock --css $HOME/sarif/markdown-css/github.css --self-contained -o design.html design.md
