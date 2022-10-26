1b7c6ec24eb3a624a4f19cf43f4155c246e7cfe7
Leon <l.botros@cs.ru.nl>
2022-10-26 15:51:24 +0200

    feat: handle "If-None-Match" and "If-Modified-Since" http headers at parameters endpoint

--- irmaseal-pkg/src/handlers/parameters.rs
+++ irmaseal-pkg/src/handlers/parameters.rs
@@ -1,21 +1,35 @@
+use actix_http::header::Header;
 use actix_web::{
-    http::header::{CacheControl, CacheDirective, ContentType, ETag, LastModified},
+    http::header::{
+        CacheControl, CacheDirective, ContentType, ETag, IfModifiedSince, IfNoneMatch, LastModified,
+    },
     web::Data,
-    HttpResponse, Responder,
+    HttpRequest, HttpResponse, Responder,
 };
 
 use crate::server::ParametersData;
 
-pub async fn parameters(pd: Data<ParametersData>) -> impl Responder
+pub async fn parameters(req: HttpRequest, pd: Data<ParametersData>) -> impl Responder
 where
 {
-    HttpResponse::Ok()
-        .insert_header(CacheControl(vec![
-            CacheDirective::Public,
-            CacheDirective::NoCache,
-        ]))
-        .insert_header(ETag(pd.etag.clone()))
-        .insert_header(LastModified(pd.last_modified.into()))
-        .content_type(ContentType::json())
-        .body(pd.pp.clone())
+    let if_none_match = IfNoneMatch::parse(&req);
+    let if_modified_since = IfModifiedSince::parse(&req);
+
+    match (if_none_match, if_modified_since) {
+        (Ok(IfNoneMatch::Items(ref tags)), ..) if tags.iter().any(|t| t.strong_eq(&pd.etag)) => {
+            HttpResponse::NotModified().finish()
+        }
+        (.., Ok(IfModifiedSince(ref since))) if &pd.last_modified <= since => {
+            HttpResponse::NotModified().finish()
+        }
+        _ => HttpResponse::Ok()
+            .insert_header(CacheControl(vec![
+                CacheDirective::Public,
+                CacheDirective::NoCache,
+            ]))
+            .insert_header(ETag(pd.etag.clone()))
+            .insert_header(LastModified(pd.last_modified))
+            .content_type(ContentType::json())
+            .body(pd.pp.clone()),
+    }
 }
