4f1e3f1b81c06ac7023bec61858403cf1ecd5362
Fay <61050421+FayKn@users.noreply.github.com>
2026-01-21 15:30:57 +0100

    SQL in docker entrypoint

---
+++ pg-pkg/api_keys.sql
@@ -0,0 +1,14 @@
+\connect "devdb";
+
+DROP TABLE IF EXISTS "api_keys";
+CREATE TABLE "public"."api_keys" (
+    "key" character varying(128) DEFAULT '' NOT NULL,
+    "email" character varying(256) NOT NULL,
+    "attributes" json DEFAULT '{}' NOT NULL,
+    "expires_at" timestamp NOT NULL,
+    CONSTRAINT "api_keys_pkey" PRIMARY KEY ("key")
+)
+WITH (oids = false);
+
+INSERT INTO "api_keys" ("key", "email", "attributes", "expires_at") VALUES
+('PG-API-hallo',	'test@test.com',	'{}',	'3000-01-08 04:05:06');
